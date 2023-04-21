910cb69b239ce670a5c51bfe283aee3a8d194c75
Matthew O'Donnell <matthew.l.odonnell@duke.edu>
2023-04-21 16:13:32 -0400

    done with second method

--- task_four.java
+++ task_four.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class task_four {
     //Implement two approaches
@@ -43,13 +44,46 @@ public class task_four {
         // OK WHAT WE GON DO IS TREAT THE X COORDINATE LIKE A TIME AND TAKE THE AVERAGE OF ALL THE Y VALUES AT THAT POINT
         // USE A THRESHOLD TO ACCOUNT FOR THE X COORDS NOT EXACTLY LINING UP (aka all x's 1-1.1 will be considered at "the same time")
         
-        ArrayList<Trajectory> simplified_set=new ArrayList<>();
-        //Simplify all Trajectories currently in our set
-        for (Trajectory p : set){
-            simplified_set.add(task_two.TS_greedy(p, 0.1));
+        ArrayList<Point> points = new ArrayList<>();
+        
+        double min_x = Double.MAX_VALUE;
+        double max_x = Double.MAX_VALUE; 
+        int max_traj_size = 0;
+
+        for(Trajectory t : set){
+            max_traj_size = Math.max(max_traj_size, t.points.size());
+            for(Point p : t.points){
+                if(p.x < min_x) min_x = p.x;
+                if(p.x > max_x) max_x = p.x;
+            }      
+        }
+        double range  = max_x - min_x;
+        double bin_width = range / max_traj_size;
+
+        double[] x_sum = new double[max_traj_size];
+        double[] y_sum = new double[max_traj_size];
+        int[] num_points = new int[max_traj_size];
+
+        for(Trajectory t : set){
+            for(Point p : t.points){
+                int bin = (int) ((p.x - min_x)/ bin_width);
+                x_sum[bin] = x_sum[bin] + p.x;
+                y_sum[bin] = y_sum[bin] + p.y;
+                num_points[bin] = num_points[bin] + 1;
+            }      
         }
 
-        return null;
+        Trajectory ret = new Trajectory("center");
+
+        for(int i = 0; i < max_traj_size; i++){
+            ret.points.add(new Point(x_sum[i] / num_points[i], y_sum[i] / num_points[i]));
+        }
+
+
+
+            
+
+        return ret;
     }
 
 
@@ -77,7 +111,7 @@ public class task_four {
         //     File_methods.createPointsFile("trajectory"+ i, set.get(i).points);
         // }
 
-        File_methods.createPointsFile("center1", first_method.points);
+        //File_methods.createPointsFile("center1", first_method.points);
         //File_methods.createPointsFile("center2", second_method.points);
         
         
