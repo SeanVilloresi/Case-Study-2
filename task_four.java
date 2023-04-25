cb2a7250e53667f1c87044867ed3bbceb5e72c1d
Matthew O'Donnell <matthew.l.odonnell@duke.edu>
2023-04-25 09:49:01 -0400

    added new center method

--- task_four.java
+++ task_four.java
@@ -103,6 +103,96 @@ public class task_four {
         return ret;
     }
 
+    public static Trajectory center3(ArrayList<Trajectory> set){
+        
+        //find the maximum length trajecotry and get its mid-point
+        int max_traj_size = 0;
+        int max_traj_dex = 0;
+        for(int i = 0; i < set.size(); i++){
+            max_traj_size = Math.max(max_traj_size, set.get(i).points.size());
+            if(set.get(i).points.size() == max_traj_size) max_traj_dex = i; 
+        }
+        int mid_dex = set.get(max_traj_dex).points.size() / 2;
+
+        //this array will store the point in each trajectory that we are pairing together
+        int[] paired_point_dexes = new int[set.size()];
+
+        //iterate over all trajectories and get the closest point to the mid-point of the longest trajectory
+        for(int i = 0; i < set.size(); i++){
+            ArrayList<Point> p = set.get(i).points;
+            double min_dist = Double.MAX_VALUE;
+            int dex = 0;
+            for(int j = 0; j < p.size(); j++){
+                double dist = set.get(max_traj_dex).points.get(mid_dex).dist(p.get(j));
+                if(dist < min_dist){
+                    min_dist = dist;
+                    dex = j;
+                }
+            }
+            //assign this point of minimum distance to the list of paired points
+            paired_point_dexes[i] = dex;
+        }
+
+        //arrays we will need for storing data for each bin
+        double[] x_sum = new double[max_traj_size*2];
+        double[] y_sum = new double[max_traj_size*2];
+        int[] num_points = new int[max_traj_size*2];
+        double[] x_avgs = new double[max_traj_size*2];
+        double[] y_avgs = new double[max_traj_size*2];
+        double[] squared_error_sums = new double[set.size()];
+        Arrays.fill(x_sum, 0);
+        Arrays.fill(y_sum, 0);
+        Arrays.fill(num_points, 0);
+        Arrays.fill(x_avgs, 0);
+        Arrays.fill(y_avgs, 0);
+        Arrays.fill(squared_error_sums, 0);
+
+        //iterate over each trajectory and add its points sequentially to the proper bucket
+        for(int i = 0; i < set.size(); i++){
+            ArrayList<Point> p = set.get(i).points;
+            //compute what bin we need to start adding the points from this trajectory
+            //make sure the point in "paired_point_dexes" ends up in the right buckets
+            int start_bin = max_traj_size - paired_point_dexes[i];
+            for(int j = 0; j < p.size(); j++){
+                int bin = start_bin + j;
+                x_sum[bin] = x_sum[bin] + p.get(j).x;
+                y_sum[bin] = y_sum[bin] + p.get(j).y;
+                num_points[bin] = num_points[bin] + 1;
+            }     
+        }
+        //create the center trajectory we will return
+        Trajectory ret = new Trajectory("center");
+
+        //iterate over all the bins... if there are points in the bin, compute their mean x and y
+        //add point to the return trajectory
+        for(int i = 0; i < max_traj_size*2; i++){
+            if(num_points[i] == 0) continue;
+            
+            ret.points.add(new Point(x_sum[i] / num_points[i], y_sum[i] / num_points[i]));
+        }
+
+        for(int i = 0; i < set.size(); i++){
+            int j = 0;
+            while(j < set.get(i).points.size() && j < ret.points.size()){
+                squared_error_sums[i] += set.get(i).points.get(j).dist(ret.points.get(j));
+                j++;
+            }
+        }
+
+        double min_avg_error = Double.MAX_VALUE;
+        int min_error_index = 0;
+
+        for(int i = 0; i < squared_error_sums.length; i++){
+            double error = squared_error_sums[i] / set.get(i).points.size();
+            if(error < min_avg_error){
+                min_avg_error = error;
+                min_error_index = i;
+            }
+        }
+
+        return set.get(min_error_index);
+    }
+
 
 
     public static void main(String[] args) throws Exception {
@@ -128,17 +218,24 @@ public class task_four {
        //Test Cases
         Trajectory first_method = center1(set);
         Trajectory second_method =center2(set);
+        Trajectory third =center3(set);
         Trajectory first_method_simp = center1(simplified_set);
         Trajectory second_method_simp =center2(simplified_set);
+        Trajectory third_simp =center2(simplified_set);
 
 
         //compute cost for second approach since it is not automatically given to us
         double sum = 0;
         double sum_simp = 0;
+        double three_sum = 0;
         for (Trajectory p : set){
             sum += task_three.dtw(p, second_method).stat;
         }
         second_method.center_cost = sum;
+        for (Trajectory p : set){
+            three_sum += task_three.dtw(p, third).stat;
+        }
+        third.center_cost = three_sum;
         for (Trajectory p : simplified_set){
             sum_simp += task_three.dtw(p, second_method_simp).stat;
         }
@@ -148,7 +245,7 @@ public class task_four {
         System.out.println("Approach II Center Cost: " + second_method.center_cost);
         System.out.println("Approach I Center Cost(Epsilon = " + epsilon + "): " + first_method_simp.center_cost);
         System.out.println("Approach II Center Cost(Epsilon = " + epsilon + "): " + second_method_simp.center_cost);
-    
+        System.out.println("Approach III Center Cost: " + third.center_cost);
 
         
         // for (int i=0; i<simplified_set.size(); i++){
